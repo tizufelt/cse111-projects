b56e28b39713c899589bab4ae4d009114c8c9d06
TimmyZ <106173110+tizufelt@users.noreply.github.com>
2023-01-28 20:12:21 -0500

    week 6 scripts

---
+++ devfun.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class emoji():
+    seenoevil = "\U0001F648"
+    hearnoevil = "\U0001F649"
+    speaknoevil = "\U0001F64A"
+    
+for monkey in emoji:
+    print(monkey.name, monkey.value)
+    
+    
